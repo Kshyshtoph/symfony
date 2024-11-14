1dc90d60ec891181a5cc4b24fd4015838119745b
Krzysztof Baum <baumkrzysztof@gmail.com>
2024-11-15 00:49:56 +0100

    more debug

--- src/Controller/GitController.php
+++ src/Controller/GitController.php
@@ -17,15 +17,17 @@ class GitController extends AbstractController
             $returnStatus = 0;
             
             // Call the upgrade.sh script and capture the output in $arr and the return status in $returnStatus
-            exec('echo "dupa"; file=$(find $HOME -name upgrade.sh); echo "dupa; echo $file; $file', $arr, $returnStatus);
-            
+            shell_exec('echo "dupa"; file=$(find $HOME -name upgrade.sh); echo "dupa; echo $file; $file', $arr, $returnStatus);
+            $arr2 = var_dump(
+            shell_exec('echo "dupa"; file=$(find $HOME -name upgrade.sh); echo "dupa; echo $file; $file', $arr, $returnStatus);
+            )
             if ($returnStatus !== 0) {
                 // Return a 500 response indicating the script was not executed successfully
-                return new Response('STH went wrong. ' . json_encode($arr), 500);
+                return new Response('STH went wrong. ' . json_encode($arr2), 500);
             }
     
             // Return a 200 response indicating the script was executed
-            return new Response('Upgrade script executed and SHA updated. ' . json_encode($arr), 200);
+            return new Response('Upgrade script executed and SHA updated. ' . json_encode($arr2), 200);
         }
     
 }
