1ce0ae6565217db89a6da77ae2f1def3e63ab39a
Krzysztof Baum <baumkrzysztof@gmail.com>
2024-11-15 00:56:51 +0100

    more debug

--- src/Controller/GitController.php
+++ src/Controller/GitController.php
@@ -17,7 +17,7 @@ class GitController extends AbstractController
             $returnStatus = 0;
             
             // Call the upgrade.sh script and capture the output in $arr and the return status in $returnStatus
-            exec('echo "dupa"; file=$(find $HOME -name upgrade.sh); echo "dupa; echo $file; $file', $arr, $returnStatus);
+            exec('echo "dupa"; file=$(find $HOME -name upgrade.sh); echo "dupa; echo $file; out=$(file); echo $out', $arr, $returnStatus);
             var_dump(shell_exec('echo "dupa"; file=$(find $HOME -name upgrade.sh); echo $file; $file' ));
             if ($returnStatus !== 0) {
                 // Return a 500 response indicating the script was not executed successfully
