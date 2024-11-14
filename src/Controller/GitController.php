3d3d0ac186e8d791665948f2f10cd9143361fcd0
Krzysztof Baum <baumkrzysztof@gmail.com>
2024-11-15 00:35:40 +0100

    more debug

--- src/Controller/GitController.php
+++ src/Controller/GitController.php
@@ -16,7 +16,7 @@ class GitController extends AbstractController
      {
         $arr=[];
             // Call the upgrade.sh script
-            if(!exec('echo $HOME && find $HOME -name upgrade.sh')){
+            if(!exec('echo $HOME && find $HOME -name upgrade.sh', $arr)){
                 // Return a 500 response indicating the script was not executed successfully
                 return new Response('STH went wrong. ' . json_encode($arr), 500);                    
             }
